b45d5b9e6dbb134ccc52dc670291c6d8dd651dc5
EthanBaratz2 <157398612+EthanBaratz2@users.noreply.github.com>
2024-09-06 11:28:54 -0400

    Update Q1WeirdFunction.java

--- src/main/java/Q1WeirdFunction.java
+++ src/main/java/Q1WeirdFunction.java
@@ -10,12 +10,19 @@ public class Q1WeirdFunction {
     }
 
     public static int fIterative(int n) {
-        if(n < 3) return n;
+           if(n < 3) return n;
+        
+        int f1 = 2;
+        int f2 = 1;
+        int f3 = 0;
         int fn = 0;
-        int j = 1;
-        for(int i = n; i > 0; i--) {
-            fn += j * (i - 1);
-            j++;
+
+        for(int i = 3; i <= n; i++) {
+            fn = f1 + 2 * f2 + 3 * f3;
+
+            f3 = f2;
+            f2 = f1;
+            f2 = fn;
         }
         return fn;
     }
